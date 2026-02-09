bd4cfbe52d913650424f2bd3ed45a358480b53d1
dilipwarrier <35469231+dilipwarrier@users.noreply.github.com>
2026-02-09 08:31:08 -0500

    Update to use Qwen3 model

--- basic_vllm.py
+++ basic_vllm.py
@@ -33,16 +33,16 @@ sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
 def main():
     # Create an LLM.
     if run_on_gpu:
-        llm = LLM(model="facebook/opt-125m")
+        llm = LLM(model="Qwen/Qwen2.5-1.5B-Instruct")
     else:
         llm = LLM(model="facebook/opt-125m",
                   dtype="bfloat16")
-    
+
     # Generate texts from the prompts.
     # The output is a list of RequestOutput objects
     # that contain the prompt, generated text, and other information.
     outputs = llm.generate(prompts, sampling_params)
-    
+
     # Print the outputs.
     print("\nGenerated Outputs:\n" + "-" * 60)
     for output in outputs:
